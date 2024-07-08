582a9a7ee6deefca0eaf495af75ac344e0376342
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-08 17:51:59 -0400

    get display to concatenate the numbers

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let x = ""
+let y = ""
+let operator = ""
+let displayValue = "0"
+let operatorFlag = false;
+
+
 const acButton = document.querySelector("#ac-btn")
 const sevenButton = document.querySelector("#seven-btn")
 const eightButton = document.querySelector("#eight-btn")
@@ -9,49 +16,146 @@ const oneButton = document.querySelector("#one-btn")
 const twoButton = document.querySelector("#two-btn")
 const threeButton = document.querySelector("#three-btn")
 const zeroButton = document.querySelector("#zero-btn")
+
 const display = document.querySelector(".display")
+display.textContent = displayValue;
 
-acButton.addEventListener("click", () => {
-  display.textContent = 0
-})
+function addButtonListeners() {
+  acButton.addEventListener("click", () => {
 
-sevenButton.addEventListener("click", () => {
-  display.textContent = 7
-})
+    // no matter what just zero out the display
+    display.textContent = "0"
+    displayValue = display.textContent
+  })
 
-eightButton.addEventListener("click", () => {
-  display.textContent = 8
-})
+  sevenButton.addEventListener("click", () => {
+    console.log(displayValue)
+    console.log(display.textContent)
+    if (displayValue === "0") {
+      display.textContent = "7"
+      displayValue = display.textContent
 
-nineButton.addEventListener("click", () => {
-  display.textContent = 7
-})
+    } else {
+      display.textContent += "7"
+      displayValue = display.textContent
+    }
+    console.log("after")
+    console.log(displayValue)
+    console.log(display.textContent)
 
-fourButton.addEventListener("click", () => {
-  display.textContent = 4
-})
+  })
 
-fiveButton.addEventListener("click", () => {
-  display.textContent = 5
-})
+  eightButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "8"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "8"
+      displayValue = display.textContent
+    }
+  })
 
-sixButton.addEventListener("click", () => {
-  display.textContent = 6
-})
+  nineButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "9"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "9"
+      displayValue = display.textContent
+    }
+  })
 
-oneButton.addEventListener("click", () => {
-  display.textContent = 1
-})
+  fourButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "4"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "4"
+      displayValue = display.textContent
+    }
+  })
 
-twoButton.addEventListener("click", () => {
-  display.textContent = 2
-})
+  fiveButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "5"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "5"
+      displayValue = display.textContent
+    }
+  })
 
-threeButton.addEventListener("click", () => {
-  display.textContent = 3
-})
+  sixButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "6"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "6"
+      displayValue = display.textContent
+    }
+  })
 
-zeroButton.addEventListener("click", () => {
-  display.textContent = 0
-})
+  oneButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "1"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "1"
+      displayValue = display.textContent
+    }
+  })
+
+  twoButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "2"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "2"
+      displayValue = display.textContent
+    }
+  })
+
+  threeButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "3"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "3"
+      displayValue = display.textContent
+    }
+  })
+
+  zeroButton.addEventListener("click", () => {
+    if (displayValue === "0") {
+      display.textContent = "0"
+      displayValue = display.textContent
+    } else {
+      display.textContent += "0"
+      displayValue = display.textContent
+    }
+  })
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function operate(firstNumber, secondNumber, operator) {
+  // calls the correct function
+}
+
+addButtonListeners();
+
 
